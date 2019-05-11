2122585cd20a2514d9e672cf6ebd1f9c4f6012f5
Alyssa Brown <alyssa.brown@live.com>
2019-05-11 09:17:03 -0600

    added to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :sessions, true
+      set :session_secret, "passwrod_security"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
+
+  get "/signup" do
+    erb :signup
+
+  post "/signup" do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save
+      session [:user_id] = user.id
+      redirect "/home"
+    else
+      redirect "/error"
+
+    get "/login" do
+      erb :login
+    end 
 
 end
